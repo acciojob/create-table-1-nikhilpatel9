9335e788c81489934356c9d9dcc5c90cbca68e24
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-06-13 11:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function insert_Row() {
     //Write your code here
-  
+   let tb=document.getElementById("sampleTable");
+	tb.innerHTML=`<tr><td>new cell1</td> 
+		<td>new cell2</td></tr>`+tb.innerHTML;
   
 }
