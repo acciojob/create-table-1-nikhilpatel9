fb9fb293704ecb35efc426f0532cc63c2eafce7b
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-06-13 11:28:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function insert_Row() {
-    //Write your code here
-   let tb=document.getElementById("sampleTable");
-	tb.innerHTML=`<tr><td>New Cell1</td> 
-		<td>New Cell2</td></tr>`+tb.innerHTML;
-  
+    let tb = document.getElementById("sampleTable");
+    tb.insertAdjacentHTML('afterbegin', `<tr>
+        <td>New Cell1</td>
+        <td>New Cell2</td>
+    </tr>`);
 }
