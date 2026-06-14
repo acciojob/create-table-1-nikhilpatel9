ee87377bbf630497fc093b7e4ca680efd9273f80
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-06-14 11:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function insert_Row() {
     let table = document.getElementById("sampleTable");
-    table.insertAdjacentHTML('afterbegin', `
-        <tr>
-            <td>New Cell1</td>
-            <td>New Cell2</td>
-        </table>
-    `);
+    let newRow = document.createElement("tr");
+    newRow.innerHTML = '<td>New Cell1</td><td>New Cell2</td>';
+    
+    if (table.rows.length > 0) {
+        table.tBodies[0].insertBefore(newRow, table.rows[0]);
+    } else {
+        table.appendChild(newRow);
+    }
 }
